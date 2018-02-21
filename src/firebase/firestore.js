0af9fe5c7a5da2aab5f9ec278d3b80ebce684266
Ritwick Dey <ritwickdey@outlook.com>
2018-02-21 19:22:08 +0530

    firestore setup

---
+++ src/firebase/firestore.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBxw5DvJVRcq1TO6W_pEgGZ9K6D8eXxnsM',
+  authDomain: 'ritwick-my-blog.firebaseapp.com',
+  databaseURL: 'https://ritwick-my-blog.firebaseio.com',
+  projectId: 'ritwick-my-blog',
+  storageBucket: 'ritwick-my-blog.appspot.com',
+  messagingSenderId: '517958639490'
+};
+
+firebase.initializeApp(config);
+
+export const db = firebase.firestore();
+export default firebase;
