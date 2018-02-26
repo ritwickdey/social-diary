5d49fd5defd3dbf5048964fb8e566e83a22ce9fe
Ritwick Dey <ritwickdey@outlook.com>
2018-02-27 01:48:21 +0530

    login component style added

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import CSSModules from 'react-css-modules';
 
 import { startLogin } from '../actions/user';
 
+import styles from '../styles/modules/login.module.css';
+
 const LoginPage = props => (
-  <div>
-    <button onClick={() => props.startLogin()}>Login with Google</button>
+  <div styleName="loginContainer" className="container">
+    <div styleName="loginScreen">
+      <div styleName="loginHeadSection">
+        <h2 styleName="loginHeadline">Login</h2>
+        <p styleName="loginSubHeadline">Just a signal click!</p>
+      </div>
+      <div styleName="loginBtns">
+        <button styleName="loginBtn_google" onClick={() => props.startLogin()}>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
   </div>
 );
 
@@ -13,4 +26,6 @@ const mapDispatchToProps = dispatch => ({
   startLogin: () => dispatch(startLogin())
 });
 
-export default connect(undefined, mapDispatchToProps)(LoginPage);
+export default connect(undefined, mapDispatchToProps)(
+  CSSModules(LoginPage, styles)
+);
