2051f4c830c4e05217b57d383240984b5e6f6539
Ritwick Dey <ritwickdey@outlook.com>
2018-02-27 18:51:04 +0530

    loading added

--- src/components/loadingPage.js
+++ src/components/loadingPage.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import CSSModules from 'react-css-modules';
+
+// https://codepen.io/ritwickdey/pen/WMdpyE
+
+import styles from '../styles/modules/loading.module.css';
 
 const LoadingPage = ({ showLoading }) => (
-  <div>{showLoading && <p>Loading...</p>}</div>
+  <div>
+    {showLoading && (
+      <div styleName="loader-root-container">
+        <div styleName="loader-container">
+          <div styleName="cicle-loader" />
+        </div>
+      </div>
+    )}
+  </div>
 );
 
 const mapStateToProp = state => ({
   showLoading: state.appStatus.showLoading
 });
 
-export default connect(mapStateToProp)(LoadingPage);
+export default connect(mapStateToProp)(CSSModules(LoadingPage, styles));
