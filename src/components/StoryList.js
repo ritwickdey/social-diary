4280013e253c6cb2c12c6b1986f9bc76e8f462df
Ritwick Dey <ritwickdey@outlook.com>
2018-02-27 13:02:36 +0530

    storylist style added & small refactoring

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StoryListItem } from './StoryListItem';
+import StoryListItem from './StoryListItem';
 import CSSModules from 'react-css-modules';
 
 import styles from '../styles/modules/storyList.module.css';
@@ -9,9 +9,13 @@ const StoryList = props => {
     <div className="container">
       <div styleName="StoryListContainer">
         <div styleName="header">
-          <div>Story Name</div>
-          <div className="hideOnExtraSmall">Author</div>
-          <div className="hideOnExtraSmall">Published Date</div>
+          <div style={{ flex: 3 }}>Story Title</div>
+          <div style={{ flex: 1 }} className="hideOnSmall hideOnExtraSmall">
+            Author
+          </div>
+          <div style={{ flex: 1 }} className="hideOnExtraSmall">
+            Published Date
+          </div>
         </div>
         {props.stories.length === 0 ? (
           <p styleName="textFormat">No Stories</p>
--- src/components/StoryListItem.js
+++ src/components/StoryListItem.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import CSSModules from 'react-css-modules';
+
+import styles from '../styles/modules/storyListItem.module.css';
 
 export const StoryListItem = props => (
-  <div>
-    <h4>
-      <Link to={(props.nextUrl || '/read/') + props.story.id}>
-        {props.story.title}
-      </Link>
-    </h4>
-    <p>{moment(props.story.postedAt).format('DD MMM YYYY')}</p>
-  </div>
+  <Link
+    styleName="noDefaultLinkUI"
+    to={(props.nextUrl || '/read/') + props.story.id}
+  >
+    <div className={styles.listItem}>
+      <p style={{ flex: 3 }}>{props.story.title}</p>
+      <p style={{ flex: 1 }} className="hideOnSmall hideOnExtraSmall">
+        {props.story.uName.split(' ')[0]}
+      </p>
+      <p style={{ flex: 1 }} className="hideOnExtraSmall ">
+        {moment(props.story.postedAt).format('DDD MMM, YY')}
+      </p>
+    </div>
+  </Link>
 );
+
+export default CSSModules(StoryListItem, styles);
