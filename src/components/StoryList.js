a592d063d2a1446e8f3eb03c4e2c769ac1967178
Ritwick Dey <ritwickdey@outlook.com>
2018-02-27 11:38:20 +0530

    story list style

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { StoryListItem } from './StoryListItem';
+import CSSModules from 'react-css-modules';
+
+import styles from '../styles/modules/storyList.module.css';
 
 const StoryList = props => {
   return (
-    <div>
-      {props.stories.length === 0 ? (
-        <p>No Stories</p>
-      ) : (
-        props.stories.map(story => (
-          <StoryListItem {...props} key={story.id} story={story} />
-        ))
-      )}
+    <div className="container">
+      <div styleName="StoryListContainer">
+        <div styleName="header">
+          <div>Story Name</div>
+          <div>Author</div>
+          <div>Published Date</div>
+        </div>
+        {props.stories.length === 0 ? (
+          <p styleName="textFormat">No Stories</p>
+        ) : (
+          props.stories.map(story => (
+            <StoryListItem {...props} key={story.id} story={story} />
+          ))
+        )}
+      </div>
     </div>
   );
 };
 
-export default StoryList;
+export default CSSModules(StoryList, styles);
